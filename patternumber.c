bff199628e227e30a64f0bd88fe477d8b38fbd35
Vijeth Kumar <71746520+Vijethkumar24@users.noreply.github.com>
2022-09-14 10:00:21 +0530

    Update patternumber.c

--- patternumber.c
+++ patternumber.c
@@ -2,16 +2,17 @@
 int main()
 {
     int n;
+    printf("Enter a number :");
     scanf("%d", &n);
-    int len = n * 2 - 1;
-    for (int i = 0; i < len; i++)
+    int Patternlen = (n * 2) - 1;
+    for (int i = 0; i < Patternlen; i++)
     {
-        for (int j = 0; j < len; j++)
+        for (int j = 0; j <Patternlen; j++)
         {
             int min = i < j ? i : j;
-            min = min < len - i ? min : len - i - 1;
-            min = min < len - j - 1 ? min : len - j - 1;
-            printf("%d ", n - min);
+            min = min < Patternlen - i ? min : Patternlen - i - 1;
+            min = min < Patternlen - j - 1 ? min : Patternlen - j - 1;
+            printf("%d\t", n - min);
         }
         printf("\n");
     }
