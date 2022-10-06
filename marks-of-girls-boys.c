0ba5d62a84929348550efd201edec0f378ddc048
Vijeth Kumar <71746520+Vijethkumar24@users.noreply.github.com>
2022-10-06 19:44:12 +0530

    Update marks-of-girls-boys.c

--- marks-of-girls-boys.c
+++ marks-of-girls-boys.c
@@ -3,10 +3,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-//Complete the following function.
-
 int marks_summation(int* marks, int number_of_students, char gender) {
-  //Write your code here.
   char b,g;
   int sum=0;
   if(gender=='b')
