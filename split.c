5fbe0466ecc34714946d4f123e316d2d0134ee45
Vijeth Kumar <71746520+Vijethkumar24@users.noreply.github.com>
2022-10-07 18:22:41 +0530

    Update split.c

--- split.c
+++ split.c
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -17,7 +11,6 @@ int main()
     s = malloc(1024 * sizeof(char));
     scanf("%[^\n]", s);
     s = realloc(s, strlen(s) + 1);
-    // Write your logic to print the tokens of the sentence here.
     for (int i = 0; i < strlen(s); i++)
     {
         if (s[i] != ' ')
