ed245b35b13ff54153bc6808b34ec740e2ac5a09
Vijeth Kumar <71746520+Vijethkumar24@users.noreply.github.com>
2022-10-09 21:24:35 +0530

    Add files via upload

---
+++ towerofhanoi.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+void towers(int, char, char, char);
+int i, top;
+float op1, op2, res;
+char symbol, postfix[20];
+float s[30];
+float value[20];
+
+float compute()
+{
+    switch (symbol)
+    {
+    case '+':
+        return op1 + op2;
+    case '-':
+        return op1 - op2;
+    case '*':
+        return op1 * op2;
+    case '/':
+        if (op2 == 0)
+        {
+            printf("divide by zero eror\n");
+            exit(0);
+        }
+        return op1 / op2;
+    case '^':
+        return pow(op1, op2);
+    }
+}
+void main()
+{
+    int choice, num;
+    while (1)
+    {
+        i = 0;
+        printf("STACK OPERATION\n");
+        printf("\n1.Evaluvate\n ");
+        printf("2-tower\n");
+        printf("3-Exit\n");
+        printf("enter your choice:");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("\nenetr suffix expression\n\n");
+            scanf("%s", postfix);
+            top = -1;
+            while (postfix[i] = '\0')
+            {
+                if (isalpha(postfix[i]))
+                {
+                    printf("enter the value for %c\n", postfix[i]);
+                    scanf("%f", &value[i]);
+                }
+                i++;
+            }
+            for (i = 0; i < strlen(postfix); i++)
+            {
+                if (isalpha(postfix[i]))
+                {
+                    s[++top] = value[i];
+                }
+            
+            else
+               {
+                op2 = s[top--];
+                op1 = s[top--];
+                res = compute();
+                s[++top] = res;
+                }
+            }
+            printf("the value of postfix expression is%f\n", res);
+            break;
+        case 2:
+            printf("enetr the no of disk:");
+            scanf("%d", &num);
+            printf("sequence is:\n");
+            towers(num, 'A', 'C', 'B');
+            break;
+        case 3:
+            return;
+        default:
+            printf("enter valid choice/n");
+        }
+    }
+}
+void towers(int num, char frompeg, char topeg, char auxpeg)
+{
+    if (num == 1)
+    {
+        printf("\n move dis 1 from peg %c to peg %c", frompeg, topeg);
+        return;
+    }
+    towers(num - 1, frompeg, auxpeg, topeg);
+    printf("/n move disk %d from peg %c to peg %c", num, frompeg, topeg);
+    towers(num - 1, auxpeg, topeg, frompeg);
+}
